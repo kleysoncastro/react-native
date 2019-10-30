3d558d52cb86770248be4a1d750ff2be3d3f212b
kleyson castro <kleysoncastro99@gmail.com>
2019-10-30 17:20:17 -0300

    start: config statusBar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import './config/ReactotronConfig'; // debug
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7155c0" />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
